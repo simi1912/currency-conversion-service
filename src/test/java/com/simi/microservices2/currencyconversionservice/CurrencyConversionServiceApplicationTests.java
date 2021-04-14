c64327c333966b38429c0500eddf5a0153ed47a9
simi1912 <nioasca@gmail.com>
2021-04-14 19:11:45 +0300

    Initial commit.

---
+++ src/test/java/com/simi/microservices2/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simi.microservices2.currencyconversionservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
